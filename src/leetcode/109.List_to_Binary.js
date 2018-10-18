3a5ac7a21bc8d2a0edc06c7c9c0da40e603b7314
Kitaharakasusa <894655469@qq.com>
2018-10-18 23:58:45 +0800

    10.18

--- src/leetcode/109.List_to_Binary.js
+++ src/leetcode/109.List_to_Binary.js
@@ -15,5 +15,26 @@ function TreeNode(val) {
  * @return {TreeNode}
  */
 var sortedListToBST = function(head) {
-    
+    if(head === null) return null;
+    return toBST(head,null);
 };
+
+/**
+ *
+ * @param {ListNode}head
+ * @param {ListNode}tail
+ */
+function toBST(head,tail) {
+    let slow = head;
+    let fast = head;
+    if( head === tail) return null;
+
+    while (fast !== tail && fast.next !== tail) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    let thead = new TreeNode(slow.val);
+    thead.left =  toBST(head, slow);
+    thead.right = toBST(slow.next, tail);
+    return thead;
+}
