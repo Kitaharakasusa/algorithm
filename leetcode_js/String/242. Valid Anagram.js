96dd730e32af930ca91c835f7630d0d68b57d91e
kitaharakasusa <894655469@qq.com>
2019-11-14 23:21:34 +0800

    u

---
+++ leetcode_js/String/242. Valid Anagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) {
+        return false;
+    }
+    let table = new Array(26).fill(0);
+    for(let i = 0; i < s.length; i++) {
+        table[s.charCodeAt(i) - 'a'.charCodeAt(0)]+=1;
+    }
+    
+    console.log(table);
+    
+    for(let i = 0; i < t.length; i++) {
+        table[t.charCodeAt(i) - 'a'.charCodeAt(0)]-=1;
+        if (table[t.charCodeAt(i) - 'a'.charCodeAt(0)]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+isAnagram("anagram", "nagaram")
